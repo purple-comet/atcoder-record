8dad33689403601ae1fe2371d28dce6f95fb7e52
purple-comet <mai1314chu@gmail.com>
2025-08-21 02:09:28 +0900

    c++の環境構築

---
+++ main.cpp
@@ -0,0 +1,14 @@
+// 本物のmain.pyは.config内 acc configから調べられる
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N;
+    cin >> N; 
+    cout << "\n";
+    return 0;
+}
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# 本物のmain.pyは.config内 acc configから調べられる
 # !/usr/bin/env pypy3
 import sys
 input = sys.stdin.readline
